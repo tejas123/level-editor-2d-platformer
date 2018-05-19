730f63803697cfda4c96a3237a29931bfd32ef6e
jayrojivadiya <jay.rojivadiya.tag@gmail.com>
2018-05-19 12:54:56 +0530

    no message

---
+++ Level Editor - 2D Platformer/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+
+	public Texture2D mapTexture;
+	public PixelToObject[] pixelColorMappings;
+
+	private Color pixelColor;
+
+	void Start ()
+	{
+		GenerateLevel ();
+	}
+
+	void GenerateLevel ()
+	{
+		// Scan whole Texture and get positions of objects
+		for (int i = 0; i < mapTexture.width; i++) {
+			for (int j = 0; j < mapTexture.height; j++) { 
+				GenerateObject (i, j);
+			}
+		}
+	}
+
+	void GenerateObject (int x, int y)
+	{
+		// Read pixel color
+		pixelColor = mapTexture.GetPixel (x, y);
+
+		if (pixelColor.a == 0) {
+			// Do nothing
+			return;
+		}
+			
+		foreach (PixelToObject pixelColorMapping in pixelColorMappings) {
+			// Scan pixelColorMappings Array for matching color maping
+			if (pixelColorMapping.pixelColor.Equals (pixelColor)) {
+				Vector2 position = new Vector2 (x, y);
+				Instantiate (pixelColorMapping.prefab, position, Quaternion.identity, transform);
+			}
+		}
+	}
+}
---
+++ Level Editor - 2D Platformer/Assets/Scripts/PixelToObject.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[System.Serializable] // To get access in Inspector
+public class PixelToObject
+{
+	public Color pixelColor;
+	public GameObject prefab;
+
+}
